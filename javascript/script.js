1f97d2cd73d5d9da20ce6ace99b760cbcf8d7aa9
Chrisanto76 <antonypoure2@gmail.com>
2022-06-02 20:50:41 +0200

    ajout des fichiers necessaires

---
+++ javascript/script.js
@@ -0,0 +1,20 @@
+$(function () {
+    $('#box').css({
+        background: 'lightblue',
+        height: '200px',
+        width: '400px',
+        padding: '30px',
+    });
+
+    $('#box').click((event) => {
+        alert('la div à été cliquée');
+        console.log(event);
+    });
+});
+
+$(function () {
+    $('h1').text('introduction à jQuery');
+    $('#content').html('<p>coucou et  chalut</p>');
+    $('#content').css('color', 'red');
+});
+
